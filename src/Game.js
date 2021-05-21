0471d8e6ce7b649f6b440ceefe35558aa68bd2d6
Rafaela Almeida Da Silva <rafaela.almeida.silva@gmail.com>
2021-05-21 11:45:02 -0400

    Tell who is winner when clicks on the square 1

--- src/Game.js
+++ src/Game.js
@@ -9,24 +9,28 @@ export default class Game extends React.Component {
       winner: null,
     };
   }
-  handleClick2() {
+  handleClick2(SqAddress) {
     //when I click I want to change the value of the state; if it is X change to O and vice versa
     if (this.state.value === "X") {
       this.setState({ value: "O" });
     } else {
       this.setState({ value: "X" });
     }
+    //determine the winner when user clicks on the square 1.
+    if (SqAddress === "1") {
+      this.setState({ winner: this.state.value });
+    }
   }
 
   render() {
     return (
       <div className="Game">
-        {this.state.winner && <h2>{this.state.winner} is winner</h2>}
+        {<h2>{this.state.winner} is winner</h2>}
         <div>
           <Square
             jogada={this.state.value}
             onClick={() => {
-              this.handleClick2();
+              this.handleClick2("1");
             }}
           />
           <Square
